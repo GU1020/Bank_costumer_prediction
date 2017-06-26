b371ffe01174ff81336bbafba9c5e02ce65bb3ae
venkateshtata <venkateshtata9@gmail.com>
2017-06-26 21:45:02 +0530

    initial commit

---
+++ ann.py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+dataset = pd.read_csv("Churn_Modelling.csv")
+X = dataset.iloc[:,3:13].values
+Y = dataset.iloc[:, 13].values
+
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X_1 = LabelEncoder()
+X[:,1] = labelencoder_X_1.fit_transform(X[:,1])
+labelencoder_X_2 = LabelEncoder()
+X[:,2] = labelencoder_X_2.fit_transform(X[:,2])
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:]
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+import keras
+
+from keras.models import Sequential
+from keras.layers import Dense
+
+classifier = Sequential()
+
+#1st hidden layer
+classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu', input_dim = 11))
+
+#2nd hidden layer
+classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu'))
+
+#output layer
+classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
+
+#compiling
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100)
+
+y_pred = classifier.predict(X_test)
+
+
+
+new_prediction = classifier.predict(sc.transform(np.array([[0.0,0,600,1,40,3,60000,2,1,1,50000]])))
+
+new_prediction = (new_prediction > 0.5)
+
+print(new_prediction)
+
+
